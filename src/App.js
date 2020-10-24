ae298dbd26b39c793e18a3c0b07fc60d170d8b11
Alberto <albertocruzadopleite@gmail.com>
2020-10-24 19:10:22 +0200

    The exercise was successfully completed

--- src/App.js
+++ src/App.js
@@ -2,9 +2,55 @@ import React from 'react';
 
 function App() {
   return (
-    <div>
-      HOLA MUNDO
-    </div>
+    <React.Fragment>
+      <div className="parent1">
+        <div className="child1"></div>
+        <div className="child2"></div>
+        <div className="child3"></div>
+      </div>
+      <div className="parent2">
+        <div className="child1"></div>
+        <div className="child2"></div>
+        <div className="child3"></div>
+      </div>
+      <div className="parent3">
+        <div className="child1"></div>
+        <div className="child2"></div>
+        <div className="child3"></div>
+      </div>
+      <div className="parent4">
+        <div className="child1"></div>
+        <div className="child2"></div>
+        <div className="child3"></div>
+      </div>
+      <div className="parent5">
+        <div className="child1"></div>
+        <div className="child2"></div>
+        <div className="child3"></div>
+      </div>
+      <div className="parent6">
+        <div className="child1"></div>
+        <div className="child2"></div>
+        <div className="child3"></div>
+        <div className="child4"></div>
+      </div>
+      <div className="parent7">
+        <div className="child1"></div>
+        <div className="child2"></div>
+        <div className="child3"></div>
+        <div className="child4"></div>
+      </div>
+      <div className="parent8">
+        <div className="child1-8"></div>
+        <div className="child2-8"></div>
+      </div>
+      <div className="parent9">
+        <div className="child1-9"></div>
+        <div className="child2-9"></div>
+        <div className="child3-9"></div>
+      </div>
+    </React.Fragment>
+    
   );
 }
 
